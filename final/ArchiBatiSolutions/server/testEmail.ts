ccb9cb08240b5f2ed0b22e51c4016ca62ab1bd8c
v7xahemd <157320316+v7xahemd@users.noreply.github.com>
2025-02-01 14:15:29 +0100

    Delete final/ArchiBatiSolutions directory

--- final/ArchiBatiSolutions/server/testEmail.ts
+++
@@ -1,8 +0,0 @@
-import { sendEmail } from "./utils/email";
-
-sendEmail({
-  subject: "Test vers Yahoo",
-  text: "Ceci est un test d'email envoyé depuis Gmail vers Yahoo.",
-})
-  .then(() => console.log("✅ Email de test envoyé !"))
-  .catch((err) => console.error("❌ Échec de l'envoi:", err));
